32028cd2e5cf79e9fe21940f3810169cf77673e1
Jennifer Deprada <jdeprada@technisys.com>
2021-11-25 13:42:07 -0300

    desafio 3

--- src/components/ItemCount/itemCount.js
+++ src/components/ItemCount/itemCount.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 
 export const ItemCount = ( {stock = 10, initial = 0} ) => {
 
-    const [cantidad, setCantidad] = useState(initial)
+    const [cantidad, setCantidad] = useState(initial) //funcion que contiene dos cosas que destructuro en cantidad y setCantidad
 
     const handleRestar = () => {
-        cantidad > initial && setCantidad( cantidad - 1 )
+        cantidad > initial && setCantidad( cantidad - 1 ) 
     }
 
     const handleSumar = () => {
@@ -13,14 +13,25 @@ export const ItemCount = ( {stock = 10, initial = 0} ) => {
     }
 
     return (
-        <div>
-            <button onClick={handleRestar}>-</button>
-            <span>{cantidad}</span>
-            <button onClick={handleSumar}>+</button>
-
-            <div>
-                <button>Agregar</button>
+        <div className="container bg-dark">
+            <div className="row p-2 text-center">
+                <div className="col">
+                    <button className="btn btn-outline-light" onClick={handleRestar}>-</button>
+                </div>
+                <div className="col">
+                    { <span className="text-white" >{cantidad}</span> }
+                </div>
+                <div className="col">
+                    <button className="btn btn-outline-light" onClick={handleSumar}>+</button>
+                </div>
+            </div>
+            <div className="row p-2">
+                <div className="col text-center">
+                    <button className="btn btn-outline-light ">Agregar</button>
+                </div>
             </div>
         </div>
+
     )
 } 
+
